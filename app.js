189f7bbfee652e027dc58ef57b8b5af5f78a3520
TheRealIzzi <israel.peck@gmail.com>
2018-01-29 13:54:58 -0600

    trying to link mLab

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var routes = require('./routes/routes.js');
 app.use('/', routes);
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/techcrunch";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
